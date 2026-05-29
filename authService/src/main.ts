9ac2e588b5c01076d0ae8755591b56e5941ce613
RayyansStuff <rayyanirfanshah05@gmail.com>
2026-05-29 14:21:09 +0700

    Added swagger API in auth service

--- authService/src/main.ts
+++ authService/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('game service')
+  .setDescription('This is the authentication service api')
+  .setVersion('1.0')
+  .addTag('auth service')
+  .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
