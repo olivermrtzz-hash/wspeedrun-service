6194d913aa48f7edc9d4c819cc609f7b3e5a0649
alifXz <alif.marsudia@gmail.com>
2026-05-30 10:20:42 +0700

    new run-service

--- src/runs/runs.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('runs')
-export class RunsController {}
--- src/runs/runs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RunsService } from './runs.service';
-import { RunsController } from './runs.controller';
-
-@Module({
-  providers: [RunsService],
-  controllers: [RunsController]
-})
-export class RunsModule {}
