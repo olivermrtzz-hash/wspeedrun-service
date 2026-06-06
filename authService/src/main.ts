afa103d712940f715b1a5bce3026ab920e0eb172
RayyansStuff <rayyanirfanshah05@gmail.com>
2026-06-06 10:58:46 +0700

    Uwoghhhh

--- authService/src/main.ts
+++ authService/src/main.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +15,16 @@ async function bootstrap() {
     transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth Service')
+    .setDescription('Auth Service API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
